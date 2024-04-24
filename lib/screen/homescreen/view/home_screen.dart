320f59b9a4f60997bfe311df8b9c9bfcb7db348c
Dipali Gunjal <dipaligunjal971@gmail.com>
2024-04-25 00:08:01 +0530

    25-04

--- lib/screen/homescreen/view/home_screen.dart
+++ lib/screen/homescreen/view/home_screen.dart
@@ -12,23 +12,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: Column(
-       children: [
-         Padding(
-           padding: const EdgeInsets.only(top: 200,left: 30,right: 30),
-           child: Container(
-             padding: EdgeInsets.symmetric(horizontal: 400),
-             height: 600,
-             width: 400,
-             child: Column(
-               children: [
-
-               ],
-             ),
-           ),
-         )
-       ],
-     ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 150),
+        child: Container(
+          height: 600,
+          width: 400,
+          // alignment: Alignment.center,
+          child:  Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Yo Man ! ',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 45,vertical: 20),
+                  child: Text(
+                    "it's a simple example of \n dark theme",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15,right: 15,top: 30),
+                  child: Container(
+                    height: 90,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Text('Light Icon',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w500),),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15,right: 15,top: 30),
+                  child: Container(
+                    height: 90,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Text('Dark Icon',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w500),),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
